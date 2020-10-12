4f49f6373f714338ec3af5cdd24dce951ec2ef4e
DESKTOP-MG1KFQA\Besim <besim.musliu@student.kyh.se>
2020-10-12 15:42:48 +0200

    Created Player, Monster and Bigmonster class. and system printlines in main. plus Switch case.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Insert input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("**********************************");
+        System.out.println("* WELCOME TO WARRIORS & MONSTERS *");
+        System.out.println("**********************************");
+        System.out.print("Enter your name: ");
+        Player player = new Player(sc.nextLine());
+        System.out.println("1. Go adventuring");
+        System.out.println("2. Show details about your character");
+        System.out.println("3. Exit game");
+
+        //Menu Choice
+        System.out.print("\nMake your choice: ");
+        switch (sc.nextLine()) {
+            case "1" -> System.out.println("Nu tryckte du på ettan");
+            case "2" -> System.out.println("nu tryckte du på tvåan");
+            case "3" -> System.out.println("nu tryckte du på trean");
+            default -> System.out.println("Du tryckte inte på något");
+        }
     }
 }
---
+++ src/com/company/Player.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Player {
+    private String playerName = "";
+    private int level = 0;
+    private int experience = 0;
+    private int healthPoints = 0;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+    public int playerLevel(int l) {
+        return this.level = l;
+    }
+
+    public int playerExperienc(int e) {
+        return this.experience = e;
+    }
+
+    public int playerhealthPoints(int h) {
+        return this.healthPoints = h;
+    }
+}
