90de27d5373ec8b3beb9bf11867cf6bde889b960
Dino Mangafic <71069904+Dyno992@users.noreply.github.com>
2020-10-12 17:37:27 +0200

    Developement processes...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        int[] findMonsterGenerator = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         //Insert input
         Scanner sc = new Scanner(System.in);
         System.out.println("**********************************");
@@ -21,7 +23,15 @@ public class Main {
         System.out.print("\nMake your choice: ");
         switch (sc.nextLine()) {
             case "1" :
-                System.out.println("Nu tryckte du på ettan");
+                int temp = 1;
+                for (int j : findMonsterGenerator) {
+                    if (temp == j) {
+                        System.out.println("Didn't find any monsters");
+                        System.out.println("[Press Enter to continue]");
+                    } else{
+                        System.out.println("Wow a wild" + Math.random(Monster));
+                    }
+                }
                 break;
             case "2" :
                 System.out.println("*****************************");
--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -6,11 +6,20 @@ public class Monster extends BigMonster {
 
     private int monsterHealthPoints = 0;
 
-    public Monster(String name, int health) {
-        super(name, health);
+    Monster monster1 = new Monster("Jebote", 50, 10);
+    Monster monster2 = new Monster("Dino", 50, 10);
+    Monster monster3 = new Monster("Besim", 50, 10);
+
+    public Monster(String name, int health, int experience) {
+        super(name, health, experience);
 //        this.monsterName = name;
 //        this.monsterHealthPoints = health;
     }
+
+    public String getMonsterName() {
+        return this.monsterName;
+    }
+
     public int monsterLevel(int l) {
         return this.monsterLevel = l;
     }
