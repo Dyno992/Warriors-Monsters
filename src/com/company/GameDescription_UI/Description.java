3937e48083ce9c5db55b55d64eee920dcc5316bd
Dino Mangafic <71069904+Dyno992@users.noreply.github.com>
2020-10-26 14:54:48 +0100

    Imports replaced with single class imports.

--- src/com/company/GameDescription_UI/Description.java
+++ src/com/company/GameDescription_UI/Description.java
@@ -1,7 +1,16 @@
 package com.company.GameDescription_UI;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
